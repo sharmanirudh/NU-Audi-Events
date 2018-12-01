5682bd235022a40affb4d5339691b9fbc4ad5c16
Anirudh Sharma <sharmanirudh27@gmail.com>
2018-12-01 11:07:20 +0530

    validate-login.js

--- docs/assets/js/validate-login.js
+++ docs/assets/js/validate-login.js
@@ -3,6 +3,11 @@ console.log("Validate js");
 var API_DOMAIN = "https://nu-audi-events.herokuapp.com";
 if (Cookies.get('NU-Audi-Events') == undefined) {
 	console.log("1");
+	console.log("window.location   " + window.location);
+	console.log(GITHUB_DOMAIN + "/login.html");
+	console.log(GITHUB_DOMAIN + "/");
+	console.log(window.location != GITHUB_DOMAIN + "/login.html" || window.location != GITHUB_DOMAIN + "/");
+
 	// if (window.location != GITHUB_DOMAIN + "/login.html" || window.location != GITHUB_DOMAIN + "/") {
 		console.log("2");
 		window.location = "./login.html";
