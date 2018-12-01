b0c38058720b3ab51480acab7d1589b38aa8440a
Anirudh Sharma <sharmanirudh27@gmail.com>
2018-12-01 10:59:57 +0530

    validate-login.js

--- docs/assets/js/validate-login.js
+++ docs/assets/js/validate-login.js
@@ -1,4 +1,4 @@
-var GITHUB_DOMAIN = "http://nu-audi-events.anirudhsharma.in";
+var GITHUB_DOMAIN = "nu-audi-events.anirudhsharma.in";
 console.log("Validate js");
 var API_DOMAIN = "https://nu-audi-events.herokuapp.com";
 if (Cookies.get('NU-Audi-Events') == undefined) {
