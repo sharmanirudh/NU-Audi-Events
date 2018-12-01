1492e0b9c3fdb2bba96a58c8b42f390192157f4f
Anirudh Sharma <sharmanirudh27@gmail.com>
2018-12-01 11:37:57 +0530

    Working flask code

--- docs/assets/js/validate-login.js
+++ docs/assets/js/validate-login.js
@@ -1,4 +1,5 @@
-var GITHUB_DOMAIN = "nu-audi-events.anirudhsharma.in";
+//var GITHUB_DOMAIN = "nu-audi-events.anirudhsharma.in";
+var GITHUB_DOMAIN = "localhost:8000/nu-audi";
 console.log("Validate js");
 var API_DOMAIN = "https://nu-audi-events.herokuapp.com";
 // if (Cookies.get('NU-Audi-Events') == undefined) {
@@ -13,34 +14,40 @@ var API_DOMAIN = "https://nu-audi-events.herokuapp.com";
 // 		window.location = "./login.html";
 // 	}
 // }
-if (Cookies.get('NU-Audi-Events') != undefined) {
-	console.log(Cookies.get());
-	var url = API_DOMAIN + '/validate-cookie';
-	var params = 'value=' + Cookies.get()["NU-Audi-Events"];
-	var request = null;
-    if (window.XMLHttpRequest) {
-        request = new XMLHttpRequest();
-     } else {
-        request = new ActiveXObject("Microsoft.XMLHTTP");
-    }
-	request.open('POST', url, true);
-	request.responseType = 'text';
-	request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	request.onload = function() {
-		var response = JSON.parse(request.responseText);
-		console.log(window.location);
-		console.log(response);
-		console.log(response["text"]);
-		if (response["text"].localeCompare("Valid cookie.") == 0) {
-			console.log("Valid Response on comparison");
-			if (window.location.href == GITHUB_DOMAIN + "/" || window.location.href == GITHUB_DOMAIN + "/login.html")
-				window.location = "./index.html";
+
+function isValidCookie() {
+	if (Cookies.get('NU-Audi-Events') != undefined) {
+		console.log(Cookies.get());
+		var url = API_DOMAIN + '/validate-cookie';
+		var params = 'value=' + Cookies.get()["NU-Audi-Events"];
+		var request = null;
+	    if (window.XMLHttpRequest) {
+	        request = new XMLHttpRequest();
+	     } else {
+	        request = new ActiveXObject("Microsoft.XMLHTTP");
+	    }
+		request.open('POST', url, true);
+		request.responseType = 'text';
+		request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		request.onload = function() {
+			var response = JSON.parse(request.responseText);
+			console.log(window.location);
+			console.log(response);
+			console.log(response["text"]);
+			if (response["text"].localeCompare("Valid cookie.") == 0) {
+				console.log("Valid Response on comparison");
+				if (window.location.href == GITHUB_DOMAIN + "/" || window.location.href == GITHUB_DOMAIN + "/login.html")
+					return true;
+			}
+			else {
+				console.log("Invalid response on comparison");
+				Cookies.remove("NU-Audi-Events");
+				return false;
+			}
 		}
-		else {
-			console.log("Invalid response on comparison");
-			Cookies.remove("NU-Audi-Events");
-			window.location = "./login.html";
-		}
+		request.send(params);
 	}
-	request.send(params);
+	else {
+		return false;
+	}	
 }
